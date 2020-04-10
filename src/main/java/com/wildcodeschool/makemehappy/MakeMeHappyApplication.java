e568cf755550324cb32ea8336c28b129af342207
bastienwcs <bastien@wldcodeschool.fr>
2020-04-10 08:45:53 +0200

    init

---
+++ src/main/java/com/wildcodeschool/makemehappy/MakeMeHappyApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.makemehappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMeHappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMeHappyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/makemehappy/MakeMeHappyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.makemehappy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMeHappyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
