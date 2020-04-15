9c6e67e0d2e5337b32e264e0e4250eab68c4b06b
swann <chevreauswann@outlook.fr>
2020-04-15 14:29:32 +0200

    début création class gift

---
+++ src/main/java/com/wildcodeschool/makemehappy/controller/giftController.java
@@ -0,0 +1,16 @@
+package com.wildcodeschool.makemehappy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class giftController {
+
+    /*@GetMapping("/test")
+    @ResponseBody
+    public Gift showTestMessage() {
+        Gift casque = new Gift(nameGift:"casque", note:3);
+        return casque;
+    }*/
+}
