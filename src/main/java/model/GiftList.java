293fb5c892c83b4041f5a55c8da82f1b0c7c117a
Alanrl22 <alan.raoul22@gmail.com>
2020-04-15 17:42:53 +0200

    Indentation ok

--- src/main/java/model/GiftList.java
+++ src/main/java/model/GiftList.java
@@ -2,23 +2,23 @@ package model;
 
 import java.util.Date;
 
-public class GiftList {
+public class GiftList{
     private String title;
     private Date date;
     private String description;
     private List<Gift> giftList;
     }
 
-    public GiftList (){
+    public GiftList () {
     }
 
-    public GiftList (String title, String description, Date date){
+    public GiftList (String title, String description, Date date) {
         this.title = title;
         this.date = date;
         this.description = description;
     }
 
-    public void setTitle (String title){
+    public void setTitle (String title) {
         this.title = title;
     }
 
@@ -26,7 +26,7 @@ public class GiftList {
         return title;
     }
 
-    public void setDate (Date date){
+    public void setDate (Date date) {
         this.date= date;
     }
 
@@ -34,13 +34,12 @@ public class GiftList {
         return date;
     }
 
-    public void setDescription (String description){
+    public void setDescription (String description) {
         this.description= description;
     }
 
     public String getDescription() {
         return description;
     }
-
 }
 
