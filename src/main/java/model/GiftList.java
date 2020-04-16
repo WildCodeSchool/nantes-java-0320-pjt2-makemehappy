e25782bf4b6d6cf8e4e06e7fe2a39939d69f50c8
Alanrl22 <alan.raoul22@gmail.com>
2020-04-16 10:21:55 +0200

    Fix

--- src/main/java/model/GiftList.java
+++ src/main/java/model/GiftList.java
@@ -2,44 +2,44 @@ package model;
 
 import java.util.Date;
 
-public class GiftList{
+public class GiftList {
     private String title;
     private Date date;
     private String description;
     private List<Gift> giftList;
+
+
+    public GiftList() {
     }
 
-    public GiftList () {
-    }
-
-    public GiftList (String title, String description, Date date) {
+    public GiftList(String title, String description, Date date) {
         this.title = title;
         this.date = date;
         this.description = description;
-    }
-
-    public void setTitle (String title) {
-        this.title = title;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setDate (Date date) {
-        this.date= date;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Date getDate() {
         return date;
     }
 
-    public void setDescription (String description) {
-        this.description= description;
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
 
