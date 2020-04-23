15e37a3697dfac42415ae2cb4665bb8df636f675
swann <chevreauswann@outlook.fr>
2020-04-24 00:01:41 +0200

    création page gift-visitor-resa-connected

--- src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
+++ src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.makemehappy.controller;
 
 import com.wildcodeschool.makemehappy.model.Gift;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,12 +21,24 @@ public class GiftController {
     }
     @GetMapping("/gift")
     public String showGift() {
+
         return "gift";
     }
 
     @GetMapping("/gift-user-reserved")
     public String showGiftUserReserved() {
+
         return "gift-user-reserved";
+    }
+
+    @GetMapping("/gift-visitor-resa-connected")
+    public String showGiftVisitorResaConnected(Model out) {
+
+        Gift gift = new Gift ( "Casque", 200.00, 3, "https://www.backmarket.fr/casque-reducteur-de-bruit-bluetooth-avec-micro-beats-studio3-wireless-noir-pas-cher/89869.html#?l=0", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Headphones_1.jpg/1200px-Headphones_1.jpg",
+                "j'en rêve la nuit!, il me le faut absolment!");
+        out.addAttribute("giftDetails", gift);
+
+        return "gift-visitor-resa-connected";
     }
 
 }
