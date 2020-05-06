ec41f075dbf7450e8cf117924c23bbe6e041fc4b
Alanrl22 <alan.raoul22@gmail.com>
2020-05-06 14:58:34 +0200

    Create DashboardController

--- src/main/java/com/wildcodeschool/makemehappy/controller/DashboardController.java
+++ src/main/java/com/wildcodeschool/makemehappy/controller/DashboardController.java
@@ -5,14 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class DashboardController {
 
 
     @GetMapping("/dashboard")
     public String showDashboard (Model model) {
-        String name =  "Donkey";
-        model.addAttribute("name", name);
+        String pseudo =  "Donkey";
+        model.addAttribute("pseudo", pseudo);
+
+        List<List> lists = new ArrayList<>();
+
+        List list1 = new List();
+
+        lists.add(list1);
+        lists.add(list2);
+        lists.add(list3);
+        lists.add(list4);
+        model.addAttribute("names", names);
         return "dashboard";
     }
 
