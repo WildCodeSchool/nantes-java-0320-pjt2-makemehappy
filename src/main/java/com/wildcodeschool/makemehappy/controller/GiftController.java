06c3ab9f4caf4827565558dbe9c6a99bbd07cfc9
swann <chevreauswann@outlook.fr>
2020-05-06 15:32:43 +0200

    mise en place méthode post pour create gift

--- src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
+++ src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
@@ -2,30 +2,49 @@ package com.wildcodeschool.makemehappy.controller;
 
 import com.wildcodeschool.makemehappy.model.Gift;
 import com.wildcodeschool.makemehappy.repository.GiftRepository;
+import org.apache.catalina.Store;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GiftController {
+
+    GiftRepository giftRepository = new GiftRepository();
 
     @GetMapping("/gift-create")
     public String showGiftCreate() {
         return "gift-create";
     }
 
+    @PostMapping("/gift-create")
+    public String postGift(Model out,
+                           @RequestParam (required = true) String nameGift,
+                           @RequestParam (required = false) String description,
+                           @RequestParam (required = false) String urlGiftPicture,
+                           @RequestParam (required = false) String urlDealer,
+                           @RequestParam (required = true) float price) {
+
+
+        out.addAttribute("gift", giftRepository.save(nameGift, description, urlGiftPicture, urlDealer, price));
+
+        return "gift-list";
+    }
+
+
     @GetMapping("/gift-list")
     public String showGiftList() {
 
         return "gift-list";
     }
-    @GetMapping("/gift")
 
+    @GetMapping("/gift")
     public String showGift(Model out) {
         String pseudo = "Bastien";
         out.addAttribute("pseudo", pseudo);
-
 
         return "gift";
     }
