08b56b2eb2d68caa72a7e8261a236304a6c6dbcc
swann <chevreauswann@outlook.fr>
2020-05-06 10:11:19 +0200

    finalisation de l'appel de la base de données pour gift

--- src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
+++ src/main/java/com/wildcodeschool/makemehappy/controller/GiftController.java
@@ -1,6 +1,7 @@
 package com.wildcodeschool.makemehappy.controller;
 
 import com.wildcodeschool.makemehappy.model.Gift;
+import com.wildcodeschool.makemehappy.repository.GiftRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,8 +39,8 @@ public class GiftController {
     @GetMapping("/gift-visitor-resa-connected")
     public String showGiftVisitorResaConnected(Model out) {
 
-        Gift gift = new Gift ( "Casque", 200.00, 3, "https://www.backmarket.fr/casque-reducteur-de-bruit-bluetooth-avec-micro-beats-studio3-wireless-noir-pas-cher/89869.html#?l=0", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Headphones_1.jpg/1200px-Headphones_1.jpg",
-                "j'en rêve la nuit!, il me le faut absolment!", "id_gift_list");
+        GiftRepository repository = new GiftRepository();
+        Gift gift = repository.findGift();
         out.addAttribute("giftDetails", gift);
 
         return "gift-visitor-resa-connected";
