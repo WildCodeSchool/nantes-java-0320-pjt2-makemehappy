da26b469f9ad03de243bfe9301469d4c033fe018
Alanrl22 <alan.raoul22@gmail.com>
2020-05-11 15:14:45 +0200

    display dashboard linked to database

--- src/main/java/com/wildcodeschool/makemehappy/controller/ListController.java
+++ src/main/java/com/wildcodeschool/makemehappy/controller/ListController.java
@@ -1,13 +1,35 @@
 package com.wildcodeschool.makemehappy.controller;
 
+import com.wildcodeschool.makemehappy.repository.GiftListRepository;
+import com.wildcodeschool.makemehappy.repository.GiftRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class ListController {
 
+    GiftListRepository giftListRepository = new GiftListRepository();
+
     @GetMapping("/list-create")
     public String showListCreate() {
+        return "list-create";
+    }
+
+    @PostMapping("/list-create")
+    public String postList(Model out,
+                           @RequestParam(required = true) String title,
+                           @RequestParam (required = true) int idTheme,
+                           @RequestParam (required = false) Date deadLine,
+                           @RequestParam (required = false) String description)
+                           {
+
+        // faire la méthode save de GiftListRepository
+        //out.addAttribute("gift", giftListRepository.save(title, deadLine, description, idTheme);
 
         return "list-create";
     }
--- src/main/java/com/wildcodeschool/makemehappy/repository/GiftListRepository.java
+++ src/main/java/com/wildcodeschool/makemehappy/repository/GiftListRepository.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.makemehappy.repository;
 
 import com.wildcodeschool.makemehappy.model.Avatar;
 import com.wildcodeschool.makemehappy.model.Gift;
+import com.wildcodeschool.makemehappy.model.GiftList;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -14,13 +15,13 @@ public class GiftListRepository {
     private static final String SQL_USER = "donkey";
     private static final String SQL_PASSWORD = "projet2$";
 
-    public List<Gift> findAllGift() {
+    public List<GiftList> findAllWishList() {
 
-        List<Gift> giftList = new ArrayList<>();
+        List<GiftList> dashboard = new ArrayList<>();
 
         try {
             Connection connection = DriverManager.getConnection(URL_DATABASE, SQL_USER, SQL_PASSWORD);
-            String request = "SELECT * FROM giftList;";
+            String request = "SELECT * FROM gift_list;";
             PreparedStatement statement = connection.prepareStatement(request);
             ResultSet resultSet = statement.executeQuery();
 
@@ -35,7 +36,18 @@ public class GiftListRepository {
                 int idUser = resultSet.getInt("id_user");
                 int idTheme = resultSet.getInt("id_theme");
 
-                Gift gift = new Gift();
+                GiftList giftList = new GiftList();
+
+                giftList.setIdGiftList(idGiftList);
+                giftList.setTitle(title);
+                giftList.setDeadLine(deadLine);
+                giftList.setShareLink(shareLink);
+                giftList.setDescription(description);
+                giftList.setNotifyGift(notifyGift);
+                giftList.setIdUser(idUser);
+                giftList.setIdTheme(idTheme);
+
+                dashboard.add(giftList);
             }
 
             connection.close();
@@ -44,6 +56,40 @@ public class GiftListRepository {
             e.printStackTrace();
         }
 
-        return giftList;
+        return dashboard;
     }
+
+
+    /*
+    public List save(String title, Date deadLine, String description, int idTheme) {
+        try {
+            Connection connection = DriverManager.getConnection(URL_DATABASE, SQL_USER, SQL_PASSWORD);
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO giftList(title, dead_line, description, id_theme) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, title);
+            statement.setDate(2, deadLine);
+            statement.setString(3, description);
+            statement.setInt(4, idTheme);
+            statement.setInt(5, 1);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to insert data");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                return new GiftList(id, title, deadLine, description);
+            }
+            else {
+                throw new SQLException("failed to get inserted id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+     */
 }
